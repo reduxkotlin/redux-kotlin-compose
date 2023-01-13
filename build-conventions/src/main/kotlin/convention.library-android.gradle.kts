0e657a93e5781d7f5e726f2acc733ab4219557ac
Martynas Petuška <martynas@petuska.dev>
2023-01-13 11:18:49 +0000

    Gha debug

--- build-conventions/src/main/kotlin/convention.library-android.gradle.kts
+++ build-conventions/src/main/kotlin/convention.library-android.gradle.kts
@@ -4,10 +4,10 @@ plugins {
 }
 
 android {
-  compileSdk = 32
+  compileSdk = 33
   defaultConfig {
     minSdk = 21
-    targetSdk = 32
+    targetSdk = 33
 //    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     publishing {
       multipleVariants {
